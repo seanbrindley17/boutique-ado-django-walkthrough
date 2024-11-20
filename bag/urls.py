04d2b23c155d7057e7211255e19820a69656279e
Sean Brindley <seanbrindley17@gmail.com>
2024-11-20 20:11:16 +0000

    Add add to bag functionality

--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,7 @@ from django.urls import path
 from . import views
 
 # One empty path to indicate route URL which will render views.index with the name of home
-urlpatterns = [path("", views.view_bag, name="view_bag")]
+urlpatterns = [
+    path("", views.view_bag, name="view_bag"),
+    path("add/<item_id>/", views.add_to_bag, name="add_to_bag"),
+]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
@@ -8,3 +8,28 @@ from django.shortcuts import render
 def view_bag(request):
 
     return render(request, "bag/bag.html")
+
+
+# Add a specified quantity of a product to the bag
+def add_to_bag(request, item_id):
+    # Need to use int() method on the quantity input to convert to integer as it will be passed as a string
+    quantity = int(request.POST.get("quantity"))
+    # Gets the redirect_url from the hidden input so the site knows where to redirect the user
+    redirect_url = request.POST.get("redirect_url")
+
+    # Variable "bag" accesses requests.session to see if the session already exists, and opens an empty dictionary if not
+    bag = request.session.get("bag", {})
+
+    # If there's already a key in the bag which matches the product ID being added
+    if item_id in list(bag.keys()):
+        # Increment the quantity by how ever many are being added
+        bag[item_id] += quantity
+    else:
+        # Creates key with items's ID which is equal to the quantity
+        bag[item_id] = quantity
+
+    # Overwrites the bag variable in the session with the updated bag variable
+    request.session["bag"] = bag
+    print(request.session["bag"])
+    # Redirects user to the redirect url from the hidden input in the form
+    return redirect(redirect_url)
